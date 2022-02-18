373db06bba8377064abc13d8d0152442479a35a6
Marina <petrukmn@yandex.ru>
2022-02-18 15:53:05 +0300

    add models and schemas to dao

--- config.py
+++ config.py
@@ -3,6 +3,7 @@
 
 # Пример
 
-# class Config(object):
-#     DEBUG = True
-#     SECRET_HERE = '249y823r9v8238r9u'
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
